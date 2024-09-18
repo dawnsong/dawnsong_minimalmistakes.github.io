c5136a1c9ea242a3bffdb24823012744ff6a765e
Xiaowei Song <dawnwei.song@gmail.com>
2024-09-17 22:39:58 -0400

    disable cookie cache for now

--- assets/xhifini.js
+++ assets/xhifini.js
@@ -38,29 +38,31 @@ function generateParam(data) {
 }
 
 function hifini(key, param){
-    var url = 'https://hifini.com/get_music.php?key=' + key + '&p=' + generateParam(param);
+    var url = 'https://hifini.com/get_music.php?key=' + key + '&p=' + generateParam(param);    
+    return url ;
+    
     console.log(url);
     var c=Cookies.get(key +'&'+ param);
-    // console.log(c);
-    // if(c == null || c == '') {
-    //     var jqxhr = $.get( url, function(result) {
-    //         // alert( "success" );
-    //         // Cookies.set(key +'&'+ param, url);
-    //         console.log(result);
-    //       })
-    //         .done(function() {
-    //           alert( "second success" );
-    //         })
-    //         .fail(function() {
-    //           alert( "error" );
-    //         })
-    //         .always(function() {
-    //           alert( "finished" );
-    //         });
-    //     // Cookies.set(key +'&'+ param, url);
-    // }else{
-    //     // url =c; 
-    // }
-    // console.log(url);    
+    console.log(c);
+    if(c == null || c == '') {
+        var jqxhr = $.get( url, function(result) {
+            // alert( "success" );
+            // Cookies.set(key +'&'+ param, url);
+            console.log(result);
+          })
+            .done(function() {
+              alert( "second success" );
+            })
+            .fail(function() {
+              alert( "error" );
+            })
+            .always(function() {
+              alert( "finished" );
+            });
+        // Cookies.set(key +'&'+ param, url);
+    }else{
+        // url =c; 
+    }
+    console.log(url);    
     return url ;
 }
